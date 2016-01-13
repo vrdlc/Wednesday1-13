ec3bc447e78b8912eef23110e2c6791caa987795
Brianna Popson and Wayne Noel <bpopson@gmail.com>
2016-01-13 13:36:46 -0800

    add lowercase function to prompts

--- js/dating.js
+++ js/dating.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-  var personality = prompt("Which is more important? Humor or Looks?");
-  var hair = prompt("Do you prefer light or dark hair color?");
+  var personality = prompt("Which is more important? Humor or Looks?").toLowerCase();
+  var hair = prompt("Do you prefer light or dark hair color?").toLowerCase();
   if (personality === "humor") {
     if (hair === "light") {
       $(".lightFunny").show();
-      $(".lightLooks").hide();
     } else if (hair === "dark") {
       $(".darkFunny").show();
-      $(".darkLooks").hide();
     }
   }
   if (personality === "looks") {
